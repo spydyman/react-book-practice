b42bfdbc5c6ad701e1748bb41a668e218e4f3d0a
ibuki <ibuki@ibuki.local>
2019-11-03 01:29:08 +0900

    配列のJSXの書き方

--- src/index.js
+++ src/index.js
@@ -3,13 +3,47 @@ import ReactDOM from 'react-dom';
 
 let dom = document.querySelector('#root');
 
-let title = '新タイトル';
-let message = '新しいメッセージです。';
+// let message_true = 'メッセージを表示します。';
+// let message_false = 'メッセージはありません。';
+
+// const msg_s = {
+//   fontsize: '20pt',
+//   fontWeight: 'bold',
+//   color: 'red',
+//   border: '1px solid blue'
+// }
+
+// let flg = true;
+
+// let el = (
+//   <div>
+//     {flg ?
+//       <p style={msg_s}>{message_true}</p>
+//       :
+//       <p style={msg_s}>{message_false}</p>
+//     }
+//   </div>
+// )
+
+// ------------------------------------------------------------------------------------
+
+const list = {
+  fontSize: '18pt',
+  padding: '5px 50px',
+}
+
+let data = [
+  <li>one</li>,
+  <li>two</li>,
+  <li>three</li>,
+]
 
 let el = (
   <div>
-    <h2>{title}</h2>
-    <p>{message}</p>
+    <ul style={list}>
+      {data}
+    </ul>
   </div>
 )
+
 ReactDOM.render(el, dom);
