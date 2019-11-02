853a904b931188c7ada8f49c4e5087c2cd19406e
ibuki <ibuki@ibuki.local>
2019-11-03 00:57:47 +0900

    JSXで書いてみる

--- public/index2.js
+++ public/index2.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 let dom = document.querySelector('#root');
-
-let title = '新タイトル';
-let message = '新しいメッセージです。';
-
 let el = (
   <div>
-    <h2>{title}</h2>
-    <p>{message}</p>
+    <h2>JSX sample</h2>
+    <p>This is sample message.</p>
   </div>
 )
 ReactDOM.render(el, dom);
