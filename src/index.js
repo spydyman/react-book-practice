0cb6ba55d7953bae3855e54f45338a08c20b5327
spydyman <y.ibuki91@gmail.com>
2019-11-04 01:16:33 +0900

    counter

--- src/index.js
+++ src/index.js
@@ -48,46 +48,70 @@ let dom = document.querySelector('#root');
 
 // ------------------------------------------------------------------------------------
 
-const table = {
-  fontSize: '16pt',
-  padding: '5px 50px',
-}
+// const table = {
+//   fontSize: '16pt',
+//   padding: '5px 50px',
+// }
 
-const th = {
-  color: 'white',
-  backgroundColor: '#006',
-  padding: '5px 15px',
-}
+// const th = {
+//   color: 'white',
+//   backgroundColor: '#006',
+//   padding: '5px 15px',
+// }
 
-const td = {
-  color: 'black',
-  padding: '5px 15px',
-  border: '1px solid gray',
-}
+// const td = {
+//   color: 'black',
+//   padding: '5px 15px',
+//   border: '1px solid gray',
+// }
 
-let data = [
-  { name: 'Taro', mail: 'taro@yamada', age: 45 },
-  { name: 'Hanako', mail: 'hanako@flower', age: 37 },
-  { name: 'Sachiko', mail: 'sachiko@happy', age: 29 },
-]
+// let data = [
+//   { name: 'Taro', mail: 'taro@yamada', age: 45 },
+//   { name: 'Hanako', mail: 'hanako@flower', age: 37 },
+//   { name: 'Sachiko', mail: 'sachiko@happy', age: 29 },
+// ]
 
-let el = (
-  <div>
-    <table style={table}>
-      <tr>
-        <th style={th}>name</th>
-        <th style={th}>mail</th>
-        <th style={th}>age</th>
-      </tr>
-      {data.map((value) => (
-        <tr>
-          <td style={td}>{value.name}</td>
-          <td style={td}>{value.mail}</td>
-          <td style={td}>{value.age}</td>
-        </tr>
-      ))}
-    </table>
-  </div>
-)
+// let el = (
+//   <div>
+//     <table style={table}>
+//       <tr>
+//         <th style={th}>name</th>
+//         <th style={th}>mail</th>
+//         <th style={th}>age</th>
+//       </tr>
+//       {data.map((value) => (
+//         <tr>
+//           <td style={td}>{value.name}</td>
+//           <td style={td}>{value.mail}</td>
+//           <td style={td}>{value.age}</td>
+//         </tr>
+//       ))}
+//     </table>
+//   </div>
+// )
 
-ReactDOM.render(el, dom);
+// ------------------------------------------------------------------------------------
+
+// const p = {
+//   fontSize: "20pt",
+//   padding: "10px",
+//   backgroundColor: "blue",
+//   color: "white",
+//   cursor: "pointer"
+// };
+
+// var counter = 0;
+
+// let doAction = (event) => {
+//   counter++;
+//   let el = (
+//     <div>
+//       <p onClick={doAction} style={p}>
+//         count: {counter}
+//       </p>
+//     </div>
+//   )
+//   ReactDOM.render(el, dom);
+// };
+
+// doAction();
