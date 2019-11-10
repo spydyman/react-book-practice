46b5b1963ad9a2d5189b0fbeb8eb7e6fcd8e8ce9
spydyman <y.ibuki91@gmail.com>
2019-11-10 23:58:11 +0900

    遊び

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
+
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            APICODE: process.env.REACT_APP_API_CODE
+            msg: 'Hello',
         }
     }
 
-    console() {
-        console.log(this.state.APICODE);
-    }
-
     render() {
-        return (
-            <div>
-                {this.console()}
-            </div>
-        )
+        return <div>
+            <p>{this.state.msg}</p>
+        </div>
     }
 }
 
