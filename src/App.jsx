8d978ee68ea192c45f46e8ae838a9e119766f566
spydyman <y.ibuki91@gmail.com>
2019-11-16 14:25:44 +0900

    一旦コミット

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,59 @@
-// import React from 'react';
-// import Button from './button';
-// import { interval } from 'rxjs';
+import React from 'react';
+import Button from './button';
+import { interval } from 'rxjs';
 
-// class App extends React.Component {
+class App extends React.Component {
 
-//     constructor(props) {
-//         super(props);
+    constructor(props) {
+        super(props);
 
-//         var start = new Date();
-//         this.state = {
-//             start: start,
-//             timeNow: '',
-//             test: '33',
-//         };
-//         this.doClick = this.doClick.bind(this);
-//     }
+        this.state = {
+        };
+        this.doClick = this.doClick.bind(this);
+    }
 
-//     doClick = () => {
-//         let now = new Date();
+    doClick = () => {
+        let now = new Date();
 
-//         this.setState(() => {
-//             // return { timeNow: now.getMilliseconds() };
-//             return {
-//                 timeNow: interval(1000).subscribe(
-//                     now.getMilliseconds()
-//                 )};
-//         });
-//     };
+        this.setState(() => {
+            // return { timeNow: now.getMilliseconds() };
+        });
+    };
 
-//     render() {
-//         return <div>
-//             <p>{this.state.timeNow}</p>
-//             <Button start={() => { this.doClick() }} ></Button>
-//         </div>;
-//     }
-// }
-
-// export default App;
-
-import React, { FunctionComponent, useEffect, useState } from 'react'
-import { interval } from 'rxjs'
-
-const App: FunctionComponent = () => {
-    const [date, setDate] = useState(new Date())
-
-    useEffect(() => {
-        const subscription = interval(1000).subscribe(() => {
-            setDate(new Date())
-        })
-        return () => {
-            subscription.unsubscribe()
-        }
-    }, [])
-
-    return (
-        <p>{`${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`}</p>
-    )
+    render() {
+        return <div>
+            <p>{this.state.timeNow}</p>
+            <Button start={() => { this.doClick() }} ></Button>
+        </div>;
+    }
 }
 
 export default App;
+
+
+
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+
+// import React, { FunctionComponent, useEffect, useState } from 'react'
+// import { interval } from 'rxjs'
+
+// const App: FunctionComponent = () => {
+//     const [date, setDate] = useState(new Date())
+
+//     useEffect(() => {
+//         const subscription = interval(1000).subscribe(() => {
+//             setDate(new Date())
+//         })
+//         return () => {
+//             subscription.unsubscribe()
+//         }
+//     }, [])
+
+//     return (
+//         <p>{`${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`}</p>
+//     )
+// }
+
+// export default App;
