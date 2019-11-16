fbe4f686cdf02c7010b3a4288c8ed90da21c694b
spydyman <y.ibuki91@gmail.com>
2019-11-16 15:41:11 +0900

    クリックで表示表示変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
 import Button from './button';
-import { interval } from 'rxjs';
+// import { interval } from 'rxjs';
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
+            flg: false,
+            curTime: '',
         };
         this.doClick = this.doClick.bind(this);
     }
 
-    doClick = () => {
-        let now = new Date();
-
-        this.setState(() => {
-            // return { timeNow: now.getMilliseconds() };
-        });
-    };
+    doClick() {
+        console.log(this.state.flg);
+        // let time = setInterval(function () { this.setState({ curTime: new Date().getSeconds().toLocaleString() }); }.bind(this), 1);
+        let time = setInterval(function () { this.setState({ curTime: new Date().getMilliseconds().toLocaleString() }); }.bind(this), 10);
+        this.setState((state) => (
+            {
+                flg: !state.flg,
+                curTime: time,
+            }
+        ));
+    }
 
     render() {
         return <div>
-            <p>{this.state.timeNow}</p>
-            <Button start={() => { this.doClick() }} ></Button>
-        </div>;
+            {/* <p onClick={this.doClick}>aaa</p> */}
+            {
+                this.state.flg === false ?
+                    ''
+                    :
+                    this.state.curTime
+            }
+            < Button start={this.doClick} ></Button >
+        </div >;
     }
 }
 
